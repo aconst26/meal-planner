a2400fe3aa64426958d69724bc679514e136aabb
Anthony Constante <85783762+aconst26@users.noreply.github.com>
2026-05-26 19:26:37 -0700

    Created Reducer & Store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.jsx'
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App store={store} />
   </StrictMode>,
 )
