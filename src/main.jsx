e1a591a639c79a575c40a769b7c727dcfed3d22e
Anthony Constante <85783762+aconst26@users.noreply.github.com>
2026-06-23 11:37:01 -0700

    React & redux maptoprops & dispatch

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './components/App.jsx'
 import { createStore } from 'redux'
 import reducer from './reducers'
+import { Provider } from 'react-redux'
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App store={store} />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </StrictMode>,
 )
